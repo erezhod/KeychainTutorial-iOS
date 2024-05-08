d0d807af47d7924b44531f81fb03f0f63bdd88f5
Erez Hod <erez@erezhod.com>
2024-05-08 14:24:15 +0300

    Initial Commit

---
+++ KeychainTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KeychainTutorial
+//
+//  Created by Erez Hod on 8/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KeychainTutorial/KeychainTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainTutorialApp.swift
+//  KeychainTutorial
+//
+//  Created by Erez Hod on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct KeychainTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
